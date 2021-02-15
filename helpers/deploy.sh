cb70c5f11ab06b79a19d191751a581cb2753e82c
Vytautas Kubilius <vytautas.kubilius@gmail.com>
2021-02-15 14:36:07 +0200

    Adjust action for using GitHub Environments.
    This commit removes the functions that drilled into the directory structure
    to determine which files changed, as that approach isn't really compatible
    with using GitHub environments. Instead, the action now expects that a
    terragrunt.hcl file exists at the root of the repository, and that its
    parameters are set via environment variables. This enables a setup where
    multiple environments are defined in GitHub, and multiple workflows are set
    up for each separate environment, with each workflow only being initiated
    if its environment variable files are changed in a PR or a push event.
    
    In addition to that, the AWS account ID and the region are now set via
    environment variables as well, simplifying the action calls.

--- helpers/deploy.sh
+++ helpers/deploy.sh
@@ -5,8 +5,6 @@
 # Required positional arguments, in order:
 # - AWS_ACCOUNT_ID : The ID of the AWS account where this is being deployed.
 # - REGION : The AWS Region where the ECS Deploy Runner exists.
-# - SOURCE_REF : The starting point for identifying all the changes. The diff between SOURCE_REF and REF will be
-#                evaluated to determine all the changed files.
 # - REPO : Repository that is being deployed.
 # - REF : The end point for identifying all the changes. The diff between SOURCE_REF and REF will be evaluated to
 #         determine all the changed files.
@@ -23,7 +21,6 @@ function invoke_infrastructure_deployer {
   local -r repo="$3"
   local -r ref="$4"
   local -r command="$5"
-  local -r deploy_path="$6"
 
   local assume_role_exports
   assume_role_exports="$(aws-auth --role-arn "arn:aws:iam::$aws_account_id:role/allow-auto-deploy-from-other-accounts" --role-duration-seconds 3600)"
@@ -36,28 +33,7 @@ function invoke_infrastructure_deployer {
   fi
 
   (eval "$assume_role_exports" && \
-    infrastructure-deployer --aws-region "$region" -- "$container" infrastructure-deploy-script --repo "$repo" --ref "$ref" --binary "terragrunt" --command "$command" --deploy-path "$deploy_path")
+    infrastructure-deployer --aws-region "$region" -- "$container" infrastructure-deploy-script --repo "$repo" --ref "$ref" --binary "terragrunt" --command "$command")
 }
 
-function run {
-  local -r aws_account_id="$1"
-  local -r region="$2"
-  local -r source_ref="$3"
-  local -r repo="$4"
-  local -r ref="$5"
-  local -r command="$6"
-
-  # We must export the function so that it can be invoked through xargs
-  export -f invoke_infrastructure_deployer
-
-  # Use git-updated-folders to find all the terragrunt modules that changed, and pipe that through to the
-  # infrastructure-deployer.
-  # The last step is a check to see if there was any output from the previous command, which will be empty if no
-  # modules were updated.
-  git-updated-folders --source-ref "$source_ref" --terragrunt --repo-root "$GITHUB_WORKSPACE" \
-    | xargs -I{} --no-run-if-empty \
-        bash -c "invoke_infrastructure_deployer \"$aws_account_id\" \"$region\" \"$repo\" \"$ref\" \"$command\" {}" \
-    |& bash -c "grep . || echo 'No terragrunt modules were updated. Skipping plan.'"
-}
-
-run "$@"
+invoke_infrastructure_deployer "$@"
